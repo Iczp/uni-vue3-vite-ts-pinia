a49d9728cb5d4e84dc97576ff5212c71220fa206
zhongpei <1000@intry.cn>
2025-07-10 10:20:51 +0800

    fade-in

--- src/pages/chat/components/Avatar.vue
+++ src/pages/chat/components/Avatar.vue
@@ -6,7 +6,9 @@
       :height="size"
       :art="src"
       :src="imageSrc"
-      class="avatar-image"
+      class="avatar-image fade-in"
+      :class="{ loaded: isImgLoaded }"
+      @load="onImgLoad"
     ></image>
     <block v-else>
       <i v-if="objectType == ObjectTypes.Room" class="i-ic:baseline-group"></i>
@@ -40,15 +42,31 @@ const imageSrc = computed(() => {
   }
   return `${props.src}`;
 });
+
+const isImgLoaded = ref(false);
 const iconClass = computed(() => {
   return '';
 });
+
+const onImgLoad = e => {
+  console.log('图片加载完成', e);
+  isImgLoaded.value = true;
+  // 图片加载完成后，将不透明度设置为1
+  // e.target.classList.add('loaded');
+};
 </script>
 <style lang="scss" scoped>
 .avatar-image {
   border-radius: 50%;
   width: 100%;
   height: 100%;
+}
+.fade-in {
+  opacity: 0;
+  transition: opacity 0.5s ease-in-out;
+}
+.loaded {
+  opacity: 1; /* 当图片加载完成时，将不透明度设置为 1 */
 }
 :where(.avatar-container) {
   display: flex;
