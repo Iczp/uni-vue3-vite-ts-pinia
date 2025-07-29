f6956c24ebfa9878a27eae0450bf52f8e51e6775
zhongpei <1000@intry.cn>
2025-07-29 11:17:17 +0800

    0

--- src/api/userHeader.ts
+++ src/api/userHeader.ts
@@ -2,7 +2,7 @@ export const userHeader = {
   'x-user-id': 'dd3803b8-4b20-4ed6-b58c-49a3e499380c',
   FaceToken: 'faceToken',
   Authorization:
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJkZDM4MDNiOC00YjIwLTRlZDYtYjU4Yy00OWEzZTQ5OTM4MGMiLCJleHAgIjoiMzMyNzkwMzc0NTkzNjU4OTk5IiwiYXBwSWQiOiJIQnVpbGRlciIsInV1aWQiOiIxMjM0NTY3ODkxMCIsImNsaWVudElkIjoid2ViIn0.JNb1qlodswNZ6awIuqlBVTmTVywOPxztPXC9wCuCxbY',
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJkZDM4MDNiOC00YjIwLTRlZDYtYjU4Yy00OWEzZTQ5OTM4MGMiLCJleHAgIjoiMzMyNzkwNjgyNTU4NDAxNzY2IiwiYXBwSWQiOiJIQnVpbGRlciIsInV1aWQiOiIxMjM0NTY3ODkxMCIsImNsaWVudElkIjoid2ViIn0.hQFxv4chus5HNFfm8PZ7zQLrh-nbV6ztLHuEBp2Vbd8',
   Intranet: true,
   Mode: '0',
   ClientId: 'web',
