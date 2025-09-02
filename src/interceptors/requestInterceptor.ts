71c42d91dbde713c0f22c47bdd382ce61e7b5193
zhongpei <1000@intry.cn>
2025-09-02 16:53:45 +0800

    App-Version

--- src/interceptors/requestInterceptor.ts
+++ src/interceptors/requestInterceptor.ts
@@ -1,9 +1,10 @@
 import { getDeviceId } from '@/utils/deviceId';
-
+import {version} from '../../package.json'
 export const _requestInterceptor = {
   async invoke(args: { url: string; header: Record<string, string> }) {
     console.log('requestInterceptor', args);
     args.header['App-Device-Id'] = await getDeviceId();
+    args.header['App-Version'] = version;
     return args;
   },
 };
